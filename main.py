c731e876ce62fcf1a719b52675bf6ce2f3cee971
divyangkhare <divyang.khare01@gmail.com>
2024-04-07 21:18:04 +0530

    added secure credit card feature.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id": str})
 df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+df_cards_security = pandas.read_csv("card_security.csv", dtype=str)
+
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -36,27 +38,38 @@ class ReservationTicket:
         """
         return content
 
+
 class Creditcard:
     def __init__(self, number):
         self.number = number
 
     def validate(self, expiration, holder, cvc):
-        card_data = {"number": self.number, "expiration": expiration,
-                     "holder": holder, "cvc": cvc}
+        card_data = {"number": self.number, "expiration": expiration, "holder": holder, "cvc": cvc}
         if card_data in df_cards:
             return True
+
+
+class SecureCreditcard(Creditcard):
+    def authenticate(self, given_password):
+        password = df_cards_security.loc[df_cards_security["number"] == self.number, "password"].squeeze()
+        if password == given_password:
+            return True
+
 
 print(df)
 hotel_ID = input("Enter ID of the Hotel: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    credit_card = Creditcard(number="1234")
+    credit_card = SecureCreditcard(number="1234567890123456")
     if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
-        hotel.book()
-        name = input("Enter your name: ")
-        reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-        print(reservation_ticket.generate())
+        if credit_card.authenticate(given_password="mypass"):
+            hotel.book()
+            name = input("Enter your name: ")
+            reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+            print(reservation_ticket.generate())
+        else:
+            print("CREDIT CARD AUTHENTICATION FAILED!!!")
     else:
         print("There was a problem with your Payment!")
 else:
